51d4e27dc6ab5052b537ace278727cf6d9f3010a
xjasz <novoselov.s@v2grp.ru>
2026-06-19 12:43:28 +0700

    feat: add grass spread logic

--- LiveLife/wwwroot/js/canvasService.js
+++ LiveLife/wwwroot/js/canvasService.js
@@ -38,55 +38,87 @@ window.canvasService = {
       const size = this.gridSize;
   
       for (let i = world.length - 1; i >= 0; i--) {
-        if (world[i].type !== 3) continue;
+        if (world[i].type === 2) {
+          this.updateGrass(world, i, size);
+        }
+        else if (world[i].type === 3) {
+          this.updateWater(world, i, size);
+        }
+      }
+    },
+
+    updateGrass: function(world, i, size) {
+      const x = i % size;
+      const y = Math.floor(i / size);
   
-        const x = i % size;
-        const y = Math.floor(i / size);
+      const dirs = [[0, -1], [-1,  0], [1,  0], [0,  1]];
   
-        const down = (y + 1) * size + x;
-
-        if (this.isEmpty(world, down)) {
-          world[down].type = 3;
-          world[i].type = 0;
+      for (let d = 0; d < dirs.length; d++) {
+        const nx = x + dirs[d][0];
+        const ny = y + dirs[d][1];
+  
+        if (nx < 0 || nx >= size || ny < 0 || ny >= size) {
           continue;
         }
-
-        const inBounds = (idx) => idx >= 0 && idx < world.length;
-
-        const downLeft = (y + 1) * size + (x - 1);
-        const downRight = (y + 1) * size + (x + 1);
-
-        const canDownLeft =
-          x > 0 &&
-          inBounds(downLeft) &&
-          this.isEmpty(world, downLeft);
-
-        const canDownRight =
-          x < size - 1 &&
-          inBounds(downRight) &&
-          this.isEmpty(world, downRight);
-
-        if (canDownLeft && canDownRight) {
-          const dir = Math.random() < 0.5 ? downLeft : downRight;
-          world[dir].type = 3;
-          world[i].type = 0;
-          continue;
+  
+        const nIndex = ny * size + nx;
+  
+        if (world[nIndex].type === 1) {
+          if (Math.random() < 0.025) {
+            world[nIndex].type = 2;
+          }
+          
+          return;
         }
+      }
+    },
+  
+    updateWater: function(world, i, size) {
+      const x = i % size;
+      const y = Math.floor(i / size);
 
-        const leftBlocked = x > 0 && !this.isEmpty(world, i - 1);
-        const rightBlocked = x < size - 1 && !this.isEmpty(world, i + 1);
+      const down = (y + 1) * size + x;
 
-        if (canDownLeft && !leftBlocked) {
-          world[downLeft].type = 3;
-          world[i].type = 0;
-          continue;
-        }
+      if (this.isEmpty(world, down)) {
+        world[down].type = 3;
+        world[i].type = 0;
+        return;
+      }
 
-        if (canDownRight && !rightBlocked) {
-          world[downRight].type = 3;
-          world[i].type = 0;
-          continue;
-        }
+      const inBounds = (idx) => idx >= 0 && idx < world.length;
+
+      const downLeft = (y + 1) * size + (x - 1);
+      const downRight = (y + 1) * size + (x + 1);
+
+      const canDownLeft =
+        x > 0 &&
+        inBounds(downLeft) &&
+        this.isEmpty(world, downLeft);
+
+      const canDownRight =
+        x < size - 1 &&
+        inBounds(downRight) &&
+        this.isEmpty(world, downRight);
+
+      if (canDownLeft && canDownRight) {
+        const dir = Math.random() < 0.5 ? downLeft : downRight;
+        world[dir].type = 3;
+        world[i].type = 0;
+        return;
+      }
+
+      const leftBlocked = x > 0 && !this.isEmpty(world, i - 1);
+      const rightBlocked = x < size - 1 && !this.isEmpty(world, i + 1);
+
+      if (canDownLeft && !leftBlocked) {
+        world[downLeft].type = 3;
+        world[i].type = 0;
+        return;
+      }
+
+      if (canDownRight && !rightBlocked) {
+        world[downRight].type = 3;
+        world[i].type = 0;
       }
     },
   
